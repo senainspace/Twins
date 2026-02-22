bc879fbbf9f56c64abc1ac0e61950ee1bafdfdc2
Hazar <kyhzr000@gmail.com>
2026-02-22 22:07:08 +0300

    update laser

--- src/Laser.java
+++ src/Laser.java
@@ -1,3 +1,13 @@
 public class Laser {
-    // A'dan B'ye doğru spread olan laser bloğunu (+) temsil eder. Robot'a neighbor olunca 50 hasar verir, 100 time unit sonra söner.
+    public int x1, y1; //laser start from A
+    public int x2, y2; //laser end at B
+
+    public Laser (int x1, int x2, int y1, int y2)
+    {
+        this.x1 = x1;
+        this.y1 = y1;
+        this.x2 = x2;
+        this.y2 = y2;
+
+    }
 }
